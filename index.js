ec56d5d9e0813a83771a48f1a0db0222886c0751
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-20 12:05:48 +0200

    connected with DB

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ var app = Express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.listen(49146, () => {
+var cors = require('cors');
+app.use(cors());
 
+var mysql = require('mysql2');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '6425',
+    database: 'mytestdb'
+})
+
+app.listen(49146, () => {
+    connection.connect(function(err){
+        if(err) throw err;
+        console.log('Connect to DB');
+    });
 });
 
 app.get('/', (req, res) =>{
