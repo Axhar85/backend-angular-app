3c28b8e755ae3bbe3bea77b12b20cf61c7cf44d9
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-20 13:49:45 +0200

    add all database methoods

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var cors = require('cors');
 app.use(cors());
 
 var mysql = require('mysql2');
+const { query, response, request } = require('express');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -26,3 +27,56 @@ app.listen(49146, () => {
 app.get('/', (req, res) =>{
     res.send('Hello World')
 });
+
+app.get('/Department',(req, res)=> {
+    var query = `SELECT * FROM Department`;
+    connection.query(query, function(err,rows,fields){
+        if(err){
+            response.send('failed');
+        }
+        response.send(rows);
+    })
+});
+app.post('/Department',(req, res)=> {
+    var query = `INSERT INTO Department
+                 (DeprtmentName)
+                  Value(?)`;
+    var values = [
+        request.body['DeprtmentName']
+    ];
+    connection.query(query, values,  function(err,rows,fields){
+        if(err){
+            response.send('failed');
+        }
+        response.json('Added Successfully');
+    })
+});
+app.put('/Department',(req, res)=> {
+    var query = `UPDATE Department
+                 SET DeprtmentName=?  WHERE DepartmentId=?`;
+                
+    var values = [
+        request.body['DeprtmentName'],
+        request.body['DeprtmentId']
+    ];
+    connection.query(query, values,  function(err,rows,fields){
+        if(err){
+            response.send('failed');
+        }
+        response.json('Updated Successfully');
+    })
+});
+app.put('/Department/:id',(req, res)=> {
+    var query = `DELETE FROM Department
+                 WHERE departmentId=?`;
+                
+    var values = [
+        parseInt(request.params.id)
+    ];
+    connection.query(query, values,  function(err,rows,fields){
+        if(err){
+            response.send('failed');
+        }
+        response.json('Deleted Successfully');
+    })
+})
