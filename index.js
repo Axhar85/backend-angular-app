b7f1b645ce7094695fdcfcecd194dc9db0f448be
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-20 11:47:32 +0200

    added packages

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ var bodyParser = require ('body-parser');
 var app = Express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
+
+app.listen(49146, () => {
+
+});
+
+app.get('/', (req, res) =>{
+    res.send('Hello World')
+});
